744db11a8c608e52d36ef0b9c2dbc1987cd6256c
Wirley Almeida <wirley.s.almeida@gmail.com>
2022-08-15 23:20:47 -0300

    first commit

---
+++ app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package br.com.dio.app.repositories.data.repositories
+
+import br.com.dio.app.repositories.data.model.Repo
+
+class RepoRepositoryImpl(private val) : RepoRepository {
+    override fun listRepositories(user: String): List<Repo> {
+        TODO("Not yet implemented")
+    }
+}
